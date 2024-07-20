5f9d09d5c7a8f7c5019b7ffb63a1c8c1e35c6ba3
minecraft <149880559+minecraft-beta@users.noreply.github.com>
2024-07-20 18:15:31 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-fetch('http://darkcryption-backend.onrender.com/log-ip', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).catch(err => console.error('Error logging IP:', err));
